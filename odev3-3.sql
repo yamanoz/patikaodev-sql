eafbe54677b0d86997dc4b252afbfe4c3755735b
yamanoz <103605491+yamanoz@users.noreply.github.com>
2022-10-07 10:12:16 +0300

    Create odev3-3.sql
    country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
    country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
    film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
    film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.

---
+++ odev3-3.sql
@@ -0,0 +1,4 @@
+SELECT country FROM country WHERE country LIKE 'A%a';
+SELECT country FROM country WHERE country LIKE '_____%n';
+SELECT * FROM film WHERE title ILIKE 't____%';
+SELECT * FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
